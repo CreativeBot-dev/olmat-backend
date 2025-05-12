feaaa77b616bd63c1461b32a968f10f07171fb13
mcramaaa <mchrama28@gmail.com>
2025-05-12 23:32:31 +0700

    everything bcs payment

--- src/database/migrations/1747065187602-keplek-not-uniqe.ts
+++ src/database/migrations/1747065187602-keplek-not-uniqe.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class KeplekNotUniqe1747065187602 implements MigrationInterface {
-    name = 'KeplekNotUniqe1747065187602'
+  name = 'KeplekNotUniqe1747065187602';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX \`IDX_2e32f17810cbc5e21972ebd7b8\` ON \`participants\``);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DROP INDEX \`IDX_2e32f17810cbc5e21972ebd7b8\` ON \`participants\``,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE UNIQUE INDEX \`IDX_2e32f17810cbc5e21972ebd7b8\` ON \`participants\` (\`keplek_id\`)`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX \`IDX_2e32f17810cbc5e21972ebd7b8\` ON \`participants\` (\`keplek_id\`)`,
+    );
+  }
 }
